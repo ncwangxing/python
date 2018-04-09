ef19d6a815b4b5f06efd65c6d8281760c569a5c7
ncwangxing <delphi_wx@sina.com>
2018-04-09 15:18:47 +0800

    15：18

--- test.py
+++ test.py
@@ -1,10 +1,14 @@
 #2018-04-09 13:20
 from sys import argv
+
 #参数的使用
-number=7
+number = 7
 print ("Hello World! \n计算题：",end='')
 print ("计算结果 25+42/7=",25+42/float(number))
-print ("我的身高: 6\'2 inch")
+
+#格式化输出
+my_height = 74 
+print ("我的身高: %d" %(my_height))
 
 filename = argv
 print ("当前文件位置：",filename)
